f424ec6e475264c284094e7f5087b1f4a0d9c9b2
raquellecampos <raquellecampos12@gmail.com>
2022-02-12 00:03:34 -0400

    pesquisa por nome

--- contatos/views.py
+++ contatos/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
+from django.core.paginator import Paginator
 
-def index(request):
-    contatos = Contato.objects.all()
+def index(request):              #All
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 2)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html',{
          'contatos': contatos
     })
@@ -11,6 +20,27 @@ def index(request):
 def ver_contato(request, contato_id):
     contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+
+    contatos = Contato.objects.order_by('-id').filter(
+        nome__icontains=termo,
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 2)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
