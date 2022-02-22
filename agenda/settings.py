765e4003fb1b34871464c4fede8b5a4aaa78fc5c
raquellecampos <raquellecampos12@gmail.com>
2022-02-22 07:07:59 -0400

    cadastro de usuário

--- agenda/settings.py
+++ agenda/settings.py
@@ -136,7 +136,7 @@ from django.contrib.messages import constants
 
 MESSAGE_TAGS = {
     constants.ERROR: 'alert-danger',
-    constants.WARNING: ' alert-warning',
+    constants.WARNING: 'alert-warning',
     constants.DEBUG: 'alert-info',
     constants.SUCCESS: 'alert-success',
     constants.INFO: 'alert-info',
